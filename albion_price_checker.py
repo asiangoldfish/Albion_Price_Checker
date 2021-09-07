edbc0039d60319d3959842373523ea9d6b484acd
Khai Duong <duongkhai@gmail.com>
2021-09-07 02:22:25 +0200

    - Successfully implemented a system that lets users find the price of an item.     -> Used the pandas module to analyse the data from the get request to the Albion data project database.

--- albion_price_checker.py
+++ albion_price_checker.py
@@ -8,6 +8,7 @@ from PIL import ImageTk, Image
 from data import item_selections, Formatted_Items_List
 import pandas as pd
 import requests
+from types import SimpleNamespace
 
 
 def loading_screen():
@@ -38,7 +39,7 @@ def get_api():
 	base_url = "https://www.albion-online-data.com/api/v2/stats/Prices/"
 
 	# Item data
-	item_id = convert_name_to_id()
+	item_id = convert_name_to_id(sub_cat_options_value, tier_value, enchant_value)
 	enchantment_level = enchant_value.get()
 	tier = tier_value.get()
 
@@ -46,21 +47,42 @@ def get_api():
 	item_query = f"{item_id}.json"
 	tier_query = f"{tier}"
 
-	print(f"{base_url}{tier}_{item_query}?locations=Caerleon")
-	print(Formatted_Items_List.items_list)
+	print(f"{base_url}{item_query}?locations=Caerleon")
 
-	# response = requests.get(f"{base_url}{item_query}?locations=Caerleon")
-	# converted_list = reformat_json(response.json())
-	# print(converted_list)
+	response = requests.get(f"{base_url}{item_query}?locations=Caerleon&qualities=2")
+	pd.set_option("display.max_columns", 11)
+	df = pd.read_json(f"{base_url}{item_query}?locations=Caerleon&qualities=2")
+	print(df.iloc[0]["sell_price_min"])
+	# sell_price_min = df.loc[df.values[0]]
 
 
 def reformat_json(obj):
+	# text = json.dumps(obj, sort_keys=True, indent=4)
 	text = json.dumps(obj, sort_keys=True, indent=4)
 	return text
 
 
-def convert_name_to_id():
-	item_id = "Item_ID"
+def convert_name_to_id(item_name, item_tier, enchant_value):
+	global tier_list
+	# Fetch input from user
+	user_input = item_name.get()
+	item_tier = item_tier.get()
+	enchant_value = enchant_value.get()
+
+	# Convert tier into rarity name, i.e. "Expert's" instead of T5
+	output_tier = ""
+	tier_ranking = ["Beginner's", "Novice's", "Journeyman's", "Adept's", "Expert's", "Master's", "Grandmaster's", "Elder's"]
+	for tier_id in range(len(tier_list)):
+		if tier_id + 1 == int(item_tier):
+			output_tier = tier_ranking[tier_id]
+
+	item_label = f"{output_tier} {user_input}"  # Combines the item name like seen in-game
+	item_id_from_dic = list(Formatted_Items_List.items_list.keys())[list(Formatted_Items_List.items_list.values()).index(item_label)]  # Retrieves the item_id. More info found here: https://stackoverflow.com/a/13149770
+
+	if enchant_value == "None":
+		item_id = f"{item_id_from_dic}"
+	else:
+		item_id = f"{item_id_from_dic}@{enchant_value}"
 
 	return item_id
 
@@ -123,7 +145,7 @@ category_options_dropdown.pack(pady=10)
 
 # Sub category list
 sub_cat_options_value = tk.StringVar()
-sub_cat_options_value.set("Item Type")
+sub_cat_options_value.set("Broadsword")
 
 sub_cat_options_list = items_list.get(category_options_value.get())
 
@@ -132,15 +154,15 @@ sub_dropdown.pack(pady=10)
 
 # Item Tier
 tier_value = tk.StringVar()
-tier_value.set("T4")
-tier_list = ["T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8"]
+tier_value.set("4")
+tier_list = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
 tier_dropdown = tk.OptionMenu(canvas, tier_value, *tier_list)
 tier_dropdown.pack(pady=10)
 
 # Enchantment list
 enchant_value = tk.StringVar()
-enchant_value.set("4")
+enchant_value.set("None")
 enchantment_list = ["None", "1", "2", "3"]
 
 enchant_dropdown = tk.OptionMenu(canvas, enchant_value, *enchantment_list)
